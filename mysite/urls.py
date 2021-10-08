0af87faa5d3a1f779c813c97da06bad02bcdf2bd
SainTurDaY27 <poom52344@gmail.com>
2021-10-08 20:53:02 +0700

    Create add-ci branch.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('', RedirectView.as_view(url='polls/', permanent=True)),
     path('polls/', include('polls.urls')),
